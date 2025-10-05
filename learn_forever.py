44e785a224caa247421dc71072e900467bc34ad6
Horizon-Research-UG <162907990+Horizon-Research-UG@users.noreply.github.com>
2025-10-06 01:39:41 +0200

    restart

--- learn_forever.py
+++
@@ -1,8 +0,0 @@
-# learn forever
-###############################
-# active recall learning with spaced repetition
-###############################
-# selfdeveloping software based on
-## Feedback Schleifen Upgrade
-
-###############################
